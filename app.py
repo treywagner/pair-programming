193dea302d0f53715f0cdc3bc215bf88a7df5a5c
Trey Wagner <treywagner@uiowa.edu>
2025-05-04 22:43:19 -0500

    WIP

---
+++ app.py
@@ -0,0 +1,10 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5003, debug=True)
